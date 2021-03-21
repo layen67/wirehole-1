4244db284de82d94dfe63ff9798531f177dc4f38
layen67 <lkbcontact@gmail.com>
2021-03-21 23:15:09 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Prereqs and docker
+sudo apt-get update &&
+    sudo apt-get install -yqq \
+        curl \
+        git \
+        apt-transport-https \
+        ca-certificates \
+        gnupg-agent \
+        software-properties-common
+
+# Install Docker repository and keys
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable" &&
+    sudo apt-get update &&
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -yqq
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+    sudo chmod +x /usr/local/bin/docker-compose &&
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# wirehole
+git clone https://github.com/layen67/wirehole-1.git &&
+    cd wirehole &&
+    docker-compose up -d
+    
+sleep 10
+apt update;
+apt install wireguard -y;
+apt install openresolv -y;
+cd /etc/wireguard;
